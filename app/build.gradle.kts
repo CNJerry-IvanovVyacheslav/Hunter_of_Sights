497e7fe96e61ec90f9a7da9e154712445a49f908
CNJerry-IvanovVyacheslav <syava11212@yandex.ru>
2025-12-11 08:40:14 +0400

    The API has been removed from public access. The old one has been deleted.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -5,6 +8,9 @@ plugins {
     alias(libs.plugins.hiltAndroid)
     id("kotlin-kapt")
 }
+
+val localProps = Properties()
+localProps.load(FileInputStream(rootProject.file("local.properties")))
 
 android {
     namespace = "com.melongamesinc.hunterofsights"
@@ -18,6 +24,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        manifestPlaceholders["GOOGLE_MAPS_KEY"] = localProps.getProperty("GOOGLE_MAPS_KEY")
     }
 
     buildTypes {
